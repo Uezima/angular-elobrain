f70b634c915d193fbecd740b3232433563431d82
Uezima <guilherme@uezima.com>
2020-05-25 17:50:04 -0300

    feat: add sidebar items

--- elobrain/src/app/layouts/dashboard/dashboard-layout.component.ts
+++ elobrain/src/app/layouts/dashboard/dashboard-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'app-dasboard-layout',
-  templateUrl: './dashboard-layout.component.html'
+  templateUrl: './dashboard-layout.component.html',
+  styleUrls: ['./dashboard-layout.component.scss']
 })
 export class DashboardLayoutComponent{}
